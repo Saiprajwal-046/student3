122cbfc65466ff7bb79219864dce841739226f8e
Saiprajwal-046 <mynumpati.saiprajwal@gmail.com>
2025-07-26 13:58:56 +0530

    Add files via upload

---
+++ Student3.java
@@ -0,0 +1,12 @@
+ class StudentDetails { 
+    public static void main(String[] args) { 
+        String name = "Disha"; 
+        String rollNo = "062"; 
+        String department = "Computer Science"; 
+ 
+        System.out.println("Student Details:"); 
+        System.out.println("Name      : " + name); 
+System.out.println("Roll No   : " + rollNo); 
+System.out.println("Department: " + department); 
+} 
+} 
